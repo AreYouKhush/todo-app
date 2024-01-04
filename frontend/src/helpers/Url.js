5ad538d35492d71f10b2f2c02af68654bd058379
AreYouKhush <khushal9587@gmail.com>
2024-01-04 13:25:05 +0530

    vercel deploy

--- frontend/src/helpers/Url.js
+++ frontend/src/helpers/Url.js
@@ -1,2 +1,2 @@
-export const backendUrl = "https://todo-app-sooty-five-23.vercel.app/"
+export const backendUrl = "https://todo-app-api-one.vercel.app/"
 // export const backendUrl = "http://localhost:3000/"
