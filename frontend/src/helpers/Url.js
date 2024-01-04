b52f1b3b93c20050cef9f19140eff2771bae0c14
AreYouKhush <khushal9587@gmail.com>
2024-01-04 19:18:30 +0530

    vercel config

--- frontend/src/helpers/Url.js
+++ frontend/src/helpers/Url.js
@@ -1,2 +1,2 @@
-// export const backendUrl = "https://todo-app-api-0op9.onrender.com/"
-export const backendUrl = "http://localhost:3000/"
+export const backendUrl = "https://todo-app-api-0op9.onrender.com/"
+// export const backendUrl = "http://localhost:3000/"
