e367346aac71deb619762d3ddaf931806dd90aab
Khushal Suthar <khushal9587@gmail.com>
2024-01-04 10:57:33 +0530

    vercel deploy

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
-const userRouter = require("./routes/user")
-const todoRouter = require("./routes/todo")
+const userRouter = require("./routes/user");
+const todoRouter = require("./routes/todo");
 const cors = require("cors");
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
-app.use(cors(
-    {
-        origin: ["https://todo-app-sooty-five-23.vercel.app/"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true
-    }
-));
-app.use('/user', userRouter);
-app.use('/todo', todoRouter);
+app.use(
+  cors({
+    origin: ["https://todo-app-sooty-five-23.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
+// app.use(cors())
+app.use("/user", userRouter);
+app.use("/todo", todoRouter);
 
 app.get("/", (req, res) => {
-    res.json({Message: "Hello, Welcome"})
-})
+  res.json({ Message: "Hello, Welcome" });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
