76d621bffc3004de23e9089ffe4866f645106185
AreYouKhush <khushal9587@gmail.com>
2024-01-04 16:46:07 +0530

    render dep

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["https://todo-app-api-one.vercel.app"],
+    origin: ["https://todo-app-eight-pi-42.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
