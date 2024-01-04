b6381cd8339e0f7c7c421173cf0d16d08526f4d9
AreYouKhush <khushal9587@gmail.com>
2024-01-04 17:00:16 +0530

    render dep

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,23 @@ const cors = require("cors");
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://your-frontend.com");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 app.use(
   cors({
     origin: ["https://todo-app-eight-pi-42.vercel.app"],
@@ -22,4 +39,4 @@ app.get("/", (req, res) => {
 app.use("/user", userRouter);
 app.use("/todo", todoRouter);
 
-module.exports = app
+module.exports = app;
